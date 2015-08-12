2b0a5715975f07a4eebc570f9fdd262b827f5fbd
Luciano Delucchi <luciano.delucchi@gmail.com>
2015-08-12 12:01:18 +0200

    Schedule the observable on the ThreadPool

--- ServiceManager.cs
+++ ServiceManager.cs
@@ -2,6 +2,7 @@
 using System.Reactive.Linq;
 using System.ServiceProcess;
 using SM.Model;
+using System.Reactive.Concurrency;
 
 namespace SM
 {
@@ -9,7 +10,7 @@ namespace SM
 	{
 		public static IObservable<IService> GetServices()
 		{
-			return ServiceController.GetServices().ToObservable().SelectMany(async s => await Service.FromServiceControllerAsync(s));
+			return ServiceController.GetServices().ToObservable(TaskPoolScheduler.Default).SelectMany(async s => await Service.FromServiceControllerAsync(s));
 		}
 	}
 }
